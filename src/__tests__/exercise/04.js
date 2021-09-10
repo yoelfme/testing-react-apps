8b0d133bbc46714df49a7d9a055ef85c3ec6db19
Yoel Monzon <yoelfme@gmail.com>
2021-09-09 20:45:21 -0500

    [update] user @jackfranklin/test-data-bot that helps us to generate data easily

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,22 +5,21 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
-import faker from 'faker'
+import { build, fake } from '@jackfranklin/test-data-bot'
 
-const buildLoginForm = (defaultValues) => {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...defaultValues
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password())
   }
-}
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
 
   render(<Login onSubmit={handleSubmit} />)
 
-  const {username, password} = buildLoginForm({ password: 'different-password'})
+  const {username, password} = buildLoginForm({ overrides: { password: 'different-password' } })
 
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
