d2fe3cf1039ff4e4e983c4b5dcf41aec8d263bdd
Yoel Monzon <yoelfme@gmail.com>
2021-09-09 19:50:43 -0500

    [update] use jest mock function to validate function calls

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -7,43 +7,20 @@ import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
 test('submitting the form calls onSubmit with username and password', () => {
-  let submittedData = null
-  const handleSubmit = (data) => (submittedData = data)
+  const handleSubmit = jest.fn()
 
   render(<Login onSubmit={handleSubmit} />)
-
-  const usernameInput = screen.getByLabelText(/username/i)
-  const passwordInput = screen.getByLabelText(/password/i)
-  const submitButton = screen.getByRole('button', { name: /submit/i})
 
   const username = 'jhon'
   const password = 'password'
   
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  userEvent.type(screen.getByLabelText(/password/i), password)
+  userEvent.click(screen.getByRole('button', { name: /submit/i}))
 
-  userEvent.type(usernameInput, username)
-  userEvent.type(passwordInput, password)
-  userEvent.click(submitButton)
-
-  expect(submittedData).toEqual({ username, password})
-  screen.debug()
+  expect(handleSubmit).toHaveBeenCalledWith({ username, password })
   
-
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use userEvent.type to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
-
+  screen.debug()
 })
 
 /*
