0bfd68dc9facad1e0930a85c95f5f7a993137899
Yoel Monzon <yoelfme@gmail.com>
2021-09-09 20:17:04 -0500

    [update] allow overrides/default values for test data

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,20 +5,23 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
-import { internet } from 'faker'
+import faker from 'faker'
 
-const buildLoginForm = () => ({
-  username: internet.userName(),
-  password: internet.password()
-})
+const buildLoginForm = (defaultValues) => {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    ...defaultValues
+  }
+}
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
 
   render(<Login onSubmit={handleSubmit} />)
 
-  const {username, password} = buildLoginForm()
-  
+  const {username, password} = buildLoginForm({ password: 'different-password'})
+
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
   userEvent.click(screen.getByRole('button', { name: /submit/i}))
