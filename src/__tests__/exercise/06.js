2ae71f678fe5547aa8fbc9ebbbb431f17d3c6946
Yoel Monzon <yoelfme@gmail.com>
2022-01-21 00:52:51 -0600

    [update] mock the complete react-use-geolocation using jest.mock

--- src/__tests__/exercise/06.js
+++ src/__tests__/exercise/06.js
@@ -3,33 +3,10 @@
 
 import * as React from 'react'
 import {render, screen, act} from '@testing-library/react'
+import { useCurrentPosition } from 'react-use-geolocation'
 import Location from '../../examples/location'
 
-beforeAll(() => {
-  window.navigator.geolocation = {
-    getCurrentPosition: jest.fn()
-  }
-})
-
-// 🐨 set window.navigator.geolocation to an object that has a getCurrentPosition mock function
-
-// 💰 I'm going to give you this handy utility function
-// it allows you to create a promise that you can resolve/reject on demand.
-function deferred() {
-  let resolve, reject
-  const promise = new Promise((res, rej) => {
-    resolve = res
-    reject = rej
-  })
-  return {promise, resolve, reject}
-}
-// 💰 Here's an example of how you use this:
-// const {promise, resolve, reject} = deferred()
-// promise.then(() => {/* do something */})
-// // do other setup stuff and assert on the pending state
-// resolve()
-// await promise
-// // assert on the resolved state
+jest.mock('react-use-geolocation')
 
 test('displays the users current location', async () => {
   const fakePosition = {
@@ -39,18 +16,21 @@ test('displays the users current location', async () => {
     }
   }
 
-  const {promise, resolve} = deferred()
+  let setReturnValue = null
+  function useMockCurrentPosition() {
+    const state = React.useState([])
+    setReturnValue = state[1]
+    return state[0]
+  }
 
-  window.navigator.geolocation.getCurrentPosition.mockImplementation(callback => {
-    promise.then(() => callback(fakePosition))
-  })
+  useCurrentPosition.mockImplementation(useMockCurrentPosition)
+
 
   render(<Location />)
   expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
 
-  await act(async () => {
-    resolve()
-    await promise
+  act(() => {
+    setReturnValue([fakePosition])
   })
 
 
